88d37542b5a5fab69c4eb81bcc19b035041b8ba3
Matheus Gomes <thematheusgomes@gmail.com>
2023-09-14 20:19:59 -0300

    fix convertion error

--- web/src/components/video-input-form.tsx
+++ web/src/components/video-input-form.tsx
@@ -51,10 +51,10 @@ export function VideoInputForm() {
       'output.mp3'
     ])
 
-    const data = ffmpeg.readFile('output.mp3')
+    const data = await ffmpeg.readFile('output.mp3')
 
-    const audioFileBlob = new Blob([data], { type: 'audio/mpeg' })
-    const audioFile = new File([audioFileBlob], 'audio.mp3', {
+    const audioFileBlob = new Blob([data], { type: 'audio/mp3' })
+    const audioFile = new File([audioFileBlob], 'output.mp3', {
       type: 'audio/mpeg'
     })
 
