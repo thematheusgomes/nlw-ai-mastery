0fd582227a87334a5a928af88c3fff7aa5425e27
Matheus Gomes <thematheusgomes@gmail.com>
2023-09-15 10:23:20 -0300

    fix headers, change field to prompt

--- api/src/routes/generate-ai-completion.ts
+++ api/src/routes/generate-ai-completion.ts
@@ -11,11 +11,11 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
 
     const bodySchema = z.object({
       videoId: z.string().uuid(),
-      template: z.string(),
+      prompt: z.string(),
       temperature: z.number().min(0).max(1).default(0.5),
     })
 
-    const { videoId, template, temperature } = bodySchema.parse(request.body)
+    const { videoId, prompt, temperature } = bodySchema.parse(request.body)
 
     const video = await prisma.video.findUniqueOrThrow({
       where: {
@@ -27,7 +27,7 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
       return reply.status(400).send({ error: 'Video transcription was not generated yet.' })
     }
 
-    const promptMessage = template.replace('{transcription}', video.transcription)
+    const promptMessage = prompt.replace('{transcription}', video.transcription)
 
     const response = await openai.chat.completions.create({
       model: 'gpt-3.5-turbo',
@@ -42,11 +42,9 @@ export async function generateAICompletionRoute(app: FastifyInstance) {
 
     streamToResponse(stream, reply.raw, {
       headers: {
-        'Accept-Control-Allow-Origin': '*',
-        'Accept-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
       }
     })
-
-    return response
   })
 }
